9c9e42cb991305151c77ec0af57605c46f69b092
Outaos <outao_eu@yahoo.com>
2022-08-03 16:01:12 +0100

    Modified

--- app.py
+++ app.py
@@ -3,24 +3,33 @@ from io import BytesIO
 from flask import Flask, render_template, request, send_file
 from flask_sqlalchemy import SQLAlchemy 
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database'    #'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database'    
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+app.debug = True
 db = SQLAlchemy(app)
+
 
 class Upload(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(50))
     data = db.Column(db.LargeBinary)
+    story = db.Column(db.String(50))
+    lng = db.Column(db.Float)          # String(50)
+    lat = db.Column(db.Float)                    # String(50)
 
-    
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         file = request.files['file']
+        add_story = request.form['story']   #args
+        add_lng = request.form['insert_longitude']    #args   lng = float(request.args['lng'])
+        add_lat = request.form['insert_latitude']    #args   lat = float(request.args['lat']) 
 
-        upload = Upload(filename=file.filename, data=file.read())
+
+        upload = Upload(filename=file.filename, data=file.read(), story=add_story, lng=add_lng, lat=add_lat)      #
         db.session.add(upload)
         db.session.commit()
 
@@ -36,3 +45,5 @@ def download(upload_id):
 @app.before_first_request
 def create_tables():
     db.create_all()
+
+    # upload = Upload(filename=file.filename, data=file.read(), name=the_name)
